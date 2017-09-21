9cfea7307f16efa200d26d44cf1335bdf4582ea1
YoungHeaKim <dydgo9507@gmail.com>
2017-09-21 16:59:46 +0900

    image.js를 작성

--- src/image.js
+++ src/image.js
@@ -18,9 +18,26 @@ const supportedImageExt = ['png', 'jpg']
 function uploadImageFile(buffer) {
   return new Promise((resolve, reject) => {
     // 파일 타입 체크해서 png, jpg가 아니면 에러 발생
-
+    const {ext, mime} = fileType(buffer)
+    // supportedImageExt.indexOf() - 보통은 indexOf를 사용하였는데 ES6에서 나온 메소드를 사용
+    if(!supportedImageExt.includes(ext)) {
+      return reject(new Error('지원하는 파일 형식이 아닙니다.'))
+    }
     // s3에 업로드 후 Location 반환
-
+    s3.upload({
+      ACL: 'public-read', // 익명의 사용자도 파일 경로만 알면 읽기 가능하도록 설정
+      Body: buffer,
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: `${uuid.v4()}.${ext}`, // 충돌을 방지하기 위해 유효아이디를 넣거나 radomString으로 넣는다.
+      ContentDisposition: 'inline',
+      ContentType: mime // mime은 문자열이다.
+    }, (err, data) => {
+      if(err) {
+        reject(err)
+      } else {
+        resolve(data.Location)
+      }
+    })
   })
 }
 
@@ -32,9 +49,12 @@ function uploadImageFile(buffer) {
 function uploadOriginalFile(file) {
   return new Promise((resolve, reject) => {
     // 1MB 보다 크면 에러 발생
-
-    // 이미지 업로드
-
+    if(file.size > 1024 * 1024) {
+      reject(new Error('파일의 크기는 1MB를 넘을 수 없습니다.'))
+    } else {
+      // 이미지 업로드
+      resolve(uploadImageFile(file.buffer))
+    }
   })
 }
 
@@ -46,7 +66,15 @@ function uploadOriginalFile(file) {
  */
 function createThumbnailJob(queue, id) {
   return new Promise((resolve, reject) => {
-
+    queue.create('thumbnail', {id})
+      .removeOnComplete(true)
+      .save(err => {
+        if(err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      })
   })
 }
 
--- src/query.js
+++ src/query.js
@@ -3,7 +3,9 @@ const knex = require('./knex')
 module.exports = {
   getCompleteImageEntries() {
     return knex('image_entry')
+      // 최초로 올린 것은 thumbnail_url에 오게 만든다.
       .whereNotNull('thumbnail_url')
+      // 제일 최신에 만들어진 것이 맨위로 오게 만든다.
       .orderBy('id', 'desc')
   },
   createImageEntry({original_url, thumbnail_url, title, description}) {
