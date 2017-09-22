b51be0ef26c24f038d93b3f0bf547d5f756286bc
YoungHeaKim <dydgo9507@gmail.com>
2017-09-22 11:25:38 +0900

    worker.js를 await로 작성하였을때

--- src/worker.js
+++ src/worker.js
@@ -37,3 +37,24 @@ queue.process('thumbnail', (job, done) => {
       })
     })
 })
+
+// // await로 코딩을 하였을 경우
+// queue.process('thumbnail', (job, done) => {
+//   const {id} = job.data
+//   try{
+//   // 이미지 항목 정보를 데이터베이스에서 가져온 후
+//   const imageEntry = await query.getImageEntryById(id)
+//   const res = await axios.get(imageEntry.original_url, {
+//         responseType: 'arrayBuffer'
+//       })
+//   const buffer = await sharp(res.data)
+//       .resize(200, 200)
+//       .crop(sharp.gravity.center)
+//       .toBuffer()
+//   const location = await image.uploadImageFile(buffer)
+//   await query.updateThumbnailUrlByid(id, location)
+//   done()
+//   }.catch(err) {
+//     done(err)
+//   }
+// })
