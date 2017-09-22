32efcfb584e06cf5fd198044bb4c72fae1aec6b0
YoungHeaKim <dydgo9507@gmail.com>
2017-09-22 11:16:52 +0900

    worker.js작성

--- src/worker.js
+++ src/worker.js
@@ -11,8 +11,29 @@ const queue = kue.createQueue()
 queue.process('thumbnail', (job, done) => {
   const {id} = job.data
   // 이미지 항목 정보를 데이터베이스에서 가져온 후
-  // 원본 이미지 다운로드
-  // 썸네일 생성
-  // 썸네일 업로드
-  // 이미지 항목의 썸네일 URL 수정
+  query.getImageEntryById(id)
+    .then(imageEntry => {
+      // 원본 이미지 다운로드
+      axios.get(imageEntry.original_url, {
+        responseType: 'arrayBuffer'
+      }).then(res => {
+        // 밑의 것을 써주지않으면 json의 형태로 가져온다.
+        // 썸네일 생성
+        return sharp(res.data)
+          .resize(200, 200)
+          .crop(sharp.gravity.center)
+          .toBuffer()
+      }).then(Buffer=> {
+        // 썸네일 업로드
+        return image.uploadImageFile(buffer)
+      }).then(location => {
+        // 이미지 항목의 썸네일 URL 수정
+         return query.updateThumbnailUrlByid(id, location)
+         // queue가 끝났는지 알아야 하기 때문에 done을 사용해준다.
+      }).then(() => {
+        done()
+      }).catch(e => {
+        done(err)
+      })
+    })
 })
